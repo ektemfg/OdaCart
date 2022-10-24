cd147f6b22d85ad5f9a1bf457c37827c56a506b3
ektemfg <ekteg@icloud.com>
2022-10-25 00:50:41 +0200

    Initial Commit

---
+++ OdaCart/OdaCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdaCartApp.swift
+//  OdaCart
+//
+//  Created by Dimitrije Pesic on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct OdaCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
